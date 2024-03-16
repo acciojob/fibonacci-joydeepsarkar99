54a256c9fd0c362fe728582381b323ae87ab84ba
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-03-16 17:24:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
-// your code here
+	let a = 0
+	let b = 1
+	let c = 1
+	if(num == 1){
+		return 0
+	}
+	else if(num == 2 || num == 3){
+		return 1
+	}
+	let i = 4
+	while(i != num+1){
+		let temp = c
+		a = b
+		b = temp
+		c = a + b
+		i++
+	}
+	return c
 }
 
 module.exports = fibonacci;
